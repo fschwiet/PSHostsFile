31a327bf77f5a64b289734fea80da8d910687c97
fschwiet <fschwiet@gmail.com>
2012-08-04 20:41:32 -0700

    removing unnecessary indirection

--- PSHostsFile/HostsFile.cs
+++ PSHostsFile/HostsFile.cs
@@ -25,7 +25,15 @@ namespace PSHostsFile
         {
             filepath = filepath ?? GetHostsPath();
 
-            new Add().AddToFile(filepath, new Add.Entry(hostName, ipAddress));
+            List<Func<IEnumerable<string>, IEnumerable<string>>> transforms = new List<Func<IEnumerable<string>, IEnumerable<string>>>();
+            
+            foreach(var entry in new HostsFile.Entry[] { new HostsFile.Entry(hostName, ipAddress)})
+            {
+                transforms.Add(Core.Remove.GetRemoveTransformForHost(entry.Hostname));
+                transforms.Add(lines => GetSetHostTransform(lines.ToArray(), entry.Hostname, entry.Address));
+            }
+
+            TransformOperation.TransformFile(filepath, transforms.ToArray());
         }
 
         public static void Remove(string hostName)
@@ -50,6 +58,54 @@ namespace PSHostsFile
             if (!File.Exists(hostsPath))
                 throw new FileNotFoundException("Hosts file not found at expected location.");
             return hostsPath;
+        }
+
+        public class Entry
+        {
+            public string Hostname;
+            public string Address;
+
+            public Entry(string hostname, string address)
+            {
+                Hostname = hostname;
+                Address = address;
+            }
+        }
+
+        public static IEnumerable<string> GetSetHostTransform(IEnumerable<string> contents, string hostName, string address)
+        {
+            List<string> result = new List<string>();
+
+            var needsInsert = true;
+
+            foreach(var line in contents)
+            {
+                if (!HostsFileUtil.IsLineAHostFilesEntry(line))
+                {
+                    result.Add(line);
+                    continue;
+                }
+ 
+                if (needsInsert)
+                {
+                    result.Add(GetHostLine(hostName, address));
+                    needsInsert = false;
+                }
+                result.Add(line);
+            }
+
+            if (needsInsert)
+            {
+                result.Add(GetHostLine(hostName, address));
+                needsInsert = false;
+            }
+
+            return result;
+        }
+
+        public static string GetHostLine(string hostName, string address)
+        {
+            return address + "\t\t" + hostName;
         }
     }
 }
